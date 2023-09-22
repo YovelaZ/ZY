48932f325927a566b7dec82c86a00d5d1072e087
Yovela <1515611743@qq.com>
2023-09-22 17:18:00 +0800

    hh

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="头像" src="./assets/zy1.png">
+  <HelloWorld msg="姓名：钟悦  学号：441521"/>
+</template>
+
+<script>
+import HelloWorld from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
